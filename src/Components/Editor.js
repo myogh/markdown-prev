748bd60a5faefa0bacb8f5d18a4a1a61765d3caf
iamamh <aungmyohtet.mlr@gmail.com>
2020-08-29 22:21:02 +0630

    completed

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../App.css';
-import Previewer from './Previewer.js'
+// import Previewer from './Previewer.js'
 import {SampleText} from './SampleText.js'
+import marked from 'marked'
 
 class Editor extends React.Component {
     constructor(){
         super()
 
         this.state = {
-            input: ""
+            input: "",
+            maximize: false
         }
         
         this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     }
 
     componentDidMount(){
@@ -26,10 +29,37 @@ class Editor extends React.Component {
         })
     }
 
+    handleClick(){
+        this.setState(prevState => {
+            return {
+                maximize: !prevState.maximize
+            }
+        })
+        console.log(this.state.maximize)
+    }
+
     render(){
+        let editorStyle
+        let prevStyle
+        if(this.state.maximize === true){
+            editorStyle = {
+                display: 'none'
+            }
+            prevStyle = {
+                width: '85%',
+                margin: 'auto'
+            }
+        } else {
+            editorStyle = {
+                display: 'flex'
+            }
+            prevStyle = {
+                width: '55%'
+            }
+        }
         return(
             <div id="mark-prev">
-                <div id="editor">
+                <div id="editor" style={editorStyle}>
                     <header id="editor-header">Editor</header>
                     <textarea 
                     name="editor" 
@@ -40,7 +70,14 @@ class Editor extends React.Component {
                     value = {this.state.input}
                     ></textarea>
                 </div>
-                <Previewer inputText = {this.state.input} />
+
+                <div id="previewer" style={prevStyle}>
+                <div id='prev-bar'>
+                    <header id="prev-header">Markdown Previewer</header>
+                    <button onClick={this.handleClick}><i  id="maxi-icon" className="far fa-window-maximize"></i></button>
+                </div>
+                <div id="prev-content" dangerouslySetInnerHTML={{__html: marked(this.state.input)}}></div>
+            </div> 
             </div>
         )
     }
--- src/Components/Previewer.js
+++ src/Components/Previewer.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import '../App.css';
+// this component was designed separately for the markdown previewer
+// But due to the addition of preview maximizing feature, this component was 
+// directly implemented in the Editor.js 
+
+import React from 'react'
+import '../App.css'
 import marked from 'marked'
 
-function Previewer(props){
-    return(
-        <div id="previewer">
-            <header id="prev-header">Markdown Previewer</header>
-            <div id="prev-content" dangerouslySetInnerHTML={{__html: marked(props.inputText)}}></div>
-        </div>
-    )
-}
+class Previewer extends React.Component {
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            maximize: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
 
+    handleClick(){
+        this.setState(prevState => {
+            return {
+                maximize: !prevState.maximize
+            }
+        })
+        console.log(this.state.maximize)
+    }
+
+    render(){
+        return(
+            <div id="previewer">
+                <div id='prev-bar'>
+                    <header id="prev-header">Markdown Previewer</header>
+                    <button onClick={this.handleClick}><i  id="maxi-icon" className="far fa-window-maximize"></i></button>
+                </div>
+                <div id="prev-content" dangerouslySetInnerHTML={{__html: marked(this.props.inputText)}}></div>
+            </div> 
+        )
+    }
+}
 export default Previewer
